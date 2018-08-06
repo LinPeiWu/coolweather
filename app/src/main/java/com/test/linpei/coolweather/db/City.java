f62e54f729ed2b510e2185543bf3c2e6039a45d9
LinPeiWu <lin852274490@qq.com>
2018-08-07 02:12:03 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/test/linpei/coolweather/db/City.java
@@ -0,0 +1,44 @@
+package com.test.linpei.coolweather.db;
+
+/**
+ * 城市
+ */
+public class City {
+
+    private int id;
+    private String cityName;//城市名字
+    private int cityCode;//城市代号
+    private int provinceId;//市所属省份id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
